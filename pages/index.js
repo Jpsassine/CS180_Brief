472b5a9231e3c3e0737ff95949f73995b6e9b337
Vraj Patel <Vraj@MacBook-Pro-8.local>
2023-02-20 14:26:19 -0800

    Added some changes to make website look better

--- pages/index.js
+++ pages/index.js
@@ -16,7 +16,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={styles.main}>
-        <div className={styles.description}>
+        <div className={styles.card}>
           <Link legacyBehavior href = '/login'>
             <a>
               Log In
