b49405c89322a521827fa6b02d976ab67bce5027
mattrogrady <42446873+mattrogrady@users.noreply.github.com>
2023-02-21 23:00:54 -0800

    Updated brieftool

--- pages/brieftool.js
+++ pages/brieftool.js
@@ -1,7 +1,36 @@
-const Login = () => {
+import Head from 'next/head'
+import { Josefin_Sans } from '@next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Josefin_Sans({ subsets: ['latin'] })
+
+export default function BriefTool () {
   return (
-    <h1> Hello World! </h1>
+    <div className={inter.className}>
+     <div className={styles.briefTool}>
+      <h1> Your daily summary </h1>
+      <div className={styles.interactionsText}>
+      <h3> Jack </h3>
+      <ul>
+       <li> Wants to know about homework due date </li>
+       <li> Recently ran into Lisa </li>
+      </ul>
+      </div>
+      <div className={styles.interactionsText}>
+      <h3> John </h3>
+      <ul>
+       <li> Sent the reports in to Trevor </li>
+       <li> Needs you to review the finance reports </li>
+      </ul>
+      </div>
+      <div className={styles.interactionsText}>
+      <h3> Karen </h3>
+      <ul>
+       <li> Wants to set up meeting on Thursday </li>
+       <li> Add to calendar? Yes/No </li>
+      </ul>
+      </div>
+     </div>
+    </div>
   );
 }
-
-export default Login;
