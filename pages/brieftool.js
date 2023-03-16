4a20874f307b8fcfa9fa81a7ce5f83efc6e9899d
mattrogrady <mattrogrady@gmail.com>
2023-03-16 16:29:01 -0700

    Merged brieftool with Vraj Patel's mypage

--- pages/brieftool.js
+++ pages/brieftool.js
@@ -15,50 +15,54 @@ const handleSubmit = () => {
   auth.signOut()
 }
 
-const Login = () => {
+const Login = ({ data }) => { // pass the data as a prop
   initFirebase();
   const auth = getAuth();
   const router = useRouter();
   const provider = new GoogleAuthProvider();
   const [user, loading] = useAuthState(auth);
-  
+
   if (loading) {
     return <div>Loading...</div>
   }
   if (user) {
-    return <div>Welcome {user.displayName}
-    <Link legacyBehavior href = '/'>
-      <button onClick={() => handleSubmit()}>Logout</button>
-    </Link>
-    <div className={inter.className}>
-     <div className={styles.briefTool}>
-      <h1> Your daily summary </h1>
-      <div className={styles.interactionsText}>
-      <h3> Jack </h3>
-      <ul>
-       <li> Wants to know about homework due date </li>
-       <li> Recently ran into Lisa </li>
-      </ul>
+    return (
+      <div>
+        <Head>
+          <title>My Page</title>
+        </Head>
+        <div>Welcome {user.displayName}
+          <Link legacyBehavior href='/'>
+            <button onClick={() => handleSubmit()}>Logout</button>
+          </Link>
+          <div className={inter.className}>
+            <div className={styles.briefTool}>
+              <h1> Your daily summary </h1>
+              {Object.entries(data).map(([key, value]) => ( // map over the data
+                <div className={styles.interactionsText} key={key}>
+                  <h3>{key}</h3>
+                  <ul>
+                    <li>{value}</li>
+                  </ul>
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
-      <div className={styles.interactionsText}>
-      <h3> John </h3>
-      <ul>
-       <li> Sent the reports in to Trevor </li>
-       <li> Needs you to review the finance reports </li>
-      </ul>
-      </div>
-      <div className={styles.interactionsText}>
-      <h3> Karen </h3>
-      <ul>
-       <li> Wants to set up meeting on Thursday </li>
-       <li> Add to calendar? Yes/No </li>
-      </ul>
-      </div>
-     </div>
-    </div>
-    </div>
+    );
   }
   return (<h1></h1>)
+}
+
+export async function getServerSideProps() {
+  const res = {
+    "John": "Hello World!",
+    "Jane": "Hello World 1!",
+    "Judy": "Hello World 2!"
+  };
+
+  return { props: { data: res } };
 }
 
 export default Login;
